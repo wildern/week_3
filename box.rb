1b04a2d1fc195fb444bd14a726ea44dcc7652e5a
bkzhn <absh.bktl@gmail.com>
2014-04-29 08:38:32 +0600

    Initial commit

---
+++ box.rb
@@ -0,0 +1,9 @@
+# write 3 classes Box, BigBox, SmallBox
+# Big and Small Boxes are inherited from Box
+# they have to have width and height attributes,
+# getters and setters and get_area() method
+# in parent class
+# get_area() has to be overriden in child classes
+
+class Box
+end
---
+++ box_spec.rb
@@ -0,0 +1,40 @@
+require "rspec"
+require "./box"
+
+describe "box" do
+  it "should return width of Box" do
+    Box.new(30, 20).get_width().should == 30
+  end
+
+	it "should return width of BigBox" do
+    BigBox.new(40, 30).get_width().should == 40
+  end
+  
+	it "should return width of SmallBox" do
+    SmallBox.new(20, 10).get_width().should == 20
+  end
+
+  it "should return height of Box" do
+    Box.new(30, 20).get_height().should == 20
+  end
+
+	it "should return height of BigBox" do
+    BigBox.new(40, 30).get_height().should == 30
+  end
+  
+	it "should return height of SmallBox" do
+    SmallBox.new(20, 10).get_height().should == 10
+  end
+
+  it "should return area of Box" do
+    Box.new(30, 20).get_area().should == 600
+  end
+
+	it "should return area of BigBox" do
+    BigBox.new(40, 30).get_area().should == "Area of big box is equal to: 1200"
+  end
+  
+	it "should return area of SmallBox" do
+    SmallBox.new(20, 10).get_area().should == "Area of small box is equal to: 200"
+  end
+end
