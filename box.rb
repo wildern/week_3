33a644877ef35c89c46f0e717dca63157f5bf11d
bkzhn <absh.bktl@gmail.com>
2014-06-06 03:57:52 +0600

    Update week_3

--- box.rb
+++ box.rb
@@ -6,4 +6,16 @@
 # get_area() has to be overriden in child classes
 
 class Box
+=begin
+	there have to be height and width attributes
+	and there have to be setter and getter methods
+	there have to be get_area() method
+=end
+	# write your code here
 end
+
+
+#TODO SmallBox
+#			override get_area() method
+#TODO BigBox
+#			override get_area() method
